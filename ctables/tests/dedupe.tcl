c54c16410a3c2024a936f12ab0954d492210c702
Jeff Lawson <jeff@bovine.net>
2013-06-17 15:08:32 -0500

    cleanup testcase to remove unrelated tests and actually generate duplicated rows

--- ctables/tests/dedupe.tcl
+++ ctables/tests/dedupe.tcl
@@ -2,93 +2,55 @@
 
 package require speedtable
 
-CExtension bullseye 1.0 {
-    CTable Pasture {
+CExtension duper 1.0 {
+    CTable Superduper {
         varstring alpha
         varstring beta indexed 1
         varstring delta
         dedupestring gamma indexed 1
     }
 
-    CTable Barn {
-        varstring alpha
-        varstring beta indexed 1
-        varstring delta
-        varstring gamma
-    }
 }
 
-package require Bullseye
+package require Duper
 
 
 if {1} {
     puts "using shmem"
-    Pasture create mypasture master name "moo3" file "mypasture.dat" size "256M"
-    Barn create mybarn master name "moo4" file "mypasture.dat" size "256M"
+    Superduper create mysuperduper master name "moo3" file "mysuperduper.dat" size "256M"
 } else {
     puts "using mem"
-    Pasture create mypasture
-    Barn create mybarn
+    Superduper create mysuperduper
 }
 
 puts "created"
 
 #
-# Store some stuff in the two tables.
+# Store some stuff in the table.
 #
 for {set i 0} {$i < 10000} {incr i} {
-    mypasture store [list alpha alfa$i beta bravo$i delta delta$i gamma golf$i]
-
-    if {$i % 150 == 0} {
-        mybarn store [list alpha alfa$i beta bravo$i delta delta$i gamma golf$i]
-    }
+	set imod10 [expr {$i % 10}]
+    mysuperduper store [list alpha alfa$i beta bravo$i delta delta$imod10 gamma golf$imod10 ]
 }
 
 puts "inserted"
 
 #
-# Query some stuff from the two tables.
+# Query some stuff from the table.
 #
-mybarn search -array barnrow -limit 10 -sort alpha -code {
+mysuperduper search -array barnrow -limit 10 -sort alpha -code {
     set count [array size barnrow]
-    if {$count != 5} {
+    if {$count != 4} {
         puts "Error: wrong number of elements (was $count, expected 5)"
     }
     parray barnrow
     puts ""
 }
 
-puts info=[mypasture share info]
-#puts pools=[mypasture share pools]
-puts free=[mypasture share free]
+puts info=[mysuperduper share info]
+#puts pools=[mysuperduper share pools]
+puts free=[mysuperduper share free]
 
-#
-# Test some shared storage.
-#
-mybarn share set alpha alfa
-if {[mybarn share get alpha] != "alfa"} {
-    error "mismatch1a"
-}
-if {[mybarn share multiget alpha] != [list "alfa"]} {
-    error "mismatch1b"
-}
-mybarn share set beta "bovo bravo"
-if {[mybarn share get beta] != "bovo bravo"} {
-    error "mismatch2a"
-}
-if {[mybarn share multiget beta] != [list "bovo bravo"]} {
-    error "mismatch2b"
-}
-mybarn share set delta deltalina gamma "gamma gamma gamma"
-if {[mybarn share get gamma] != "gamma gamma gamma"} {
-    error "mismatch3a"
-}
-if {[mybarn share multiget gamma] != [list "gamma gamma gamma"]} {
-    error "mismatch3b"
-}
-if {[mybarn share multiget beta delta] != [list "bovo bravo" "deltalina"]} {
-    error "mismatch4"
-}
 
 
 
